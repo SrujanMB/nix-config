c16012f6c6eb18a80be541e8510131237878ca4a
Srujan MB <149997229+SrujanMB@users.noreply.github.com>
2025-01-13 16:21:17 +1300

    Added new profile for new system

--- flake.nix
+++ flake.nix
@@ -4,37 +4,47 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-  };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
-      srujan = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/srujan/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-
-      dev-vm = nixpkgs.lib.nixosSystem {
-         specialArgs = {inherit inputs;};
-         modules = [
-           ./hosts/dev-vm/configuration.nix
-           inputs.home-manager.nixosModules.default
-         ];
-       };
-
-      dev-vm-nogui = nixpkgs.lib.nixosSystem {
-         specialArgs = {inherit inputs;};
-         modules = [
-           ./hosts/dev-vm-nogui/configuration.nix
-           inputs.home-manager.nixosModules.default
-         ];
-       };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations = {
+        srujan = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/srujan/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        srujan-home = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/srujan-home/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        dev-vm = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/dev-vm/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        dev-vm-nogui = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/dev-vm-nogui/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+      };
+    };
 }
---
+++ hosts/srujan-home/home.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../../modules/home-manager/base/bundle.nix
+    ../../modules/home-manager/desktop/bundle.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "srujan";
+  home.homeDirectory = "/home/srujan";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # Add extra packages with "pkgs.{package-name}" syntax
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {};
+
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
