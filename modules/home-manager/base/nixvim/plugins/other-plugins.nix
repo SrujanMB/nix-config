f614b54eb6eda5aefa49d151e6fa49f783138b41
Srujan MB <149997229+SrujanMB@users.noreply.github.com>
2025-01-24 16:14:07 +1300

    Fix a mistake in other-plugins

--- modules/home-manager/base/nixvim/plugins/other-plugins.nix
+++ modules/home-manager/base/nixvim/plugins/other-plugins.nix
@@ -14,7 +14,7 @@
       # Add indentation guides even on blank lines
       # For configuration see `:help ibl`
       # https://nix-community.github.io/nixvim/plugins/indent-blankline/index.html
-      plugins.indent-blankline = {
+      indent-blankline = {
         enable = true;
       };
 
