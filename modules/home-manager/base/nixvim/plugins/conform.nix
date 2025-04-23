98f351153eaf74d97868cdf1f449adb7d9de90ab
Srujan MB <149997229+SrujanMB@users.noreply.github.com>
2025-04-23 19:20:36 +1200

    Disabled format on save because java problems

--- modules/home-manager/base/nixvim/plugins/conform.nix
+++ modules/home-manager/base/nixvim/plugins/conform.nix
@@ -17,18 +17,18 @@
       enable = true;
       settings = {
         notify_on_error = false;
-        format_on_save = ''
-          function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for lanuages that don't
-            -- have a well standardized coding style. You can add additional
-            -- lanuages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-              timeout_ms = 500,
-              lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
-            }
-          end
-        '';
+        # format_on_save = ''
+        #   function(bufnr)
+        #     -- Disable "format_on_save lsp_fallback" for lanuages that don't
+        #     -- have a well standardized coding style. You can add additional
+        #     -- lanuages here or re-enable it for the disabled ones.
+        #     local disable_filetypes = { c = true, cpp = true }
+        #     return {
+        #       timeout_ms = 500,
+        #       lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype]
+        #     }
+        #   end
+        # '';
         formatters_by_ft = {
           lua = [ "stylua" ];
 
